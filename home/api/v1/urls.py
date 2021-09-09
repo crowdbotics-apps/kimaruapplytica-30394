50695208544ed3aeb0b7f913322713e1f9a75bb5
Kimaru thagana <macbuntu@localhost.localdomain>
2021-09-09 11:08:51 +0300

    add model routers and link to viewsets

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -4,11 +4,17 @@ from rest_framework.routers import DefaultRouter
 from home.api.v1.viewsets import (
     SignupViewSet,
     LoginViewSet,
+    AppViewSet,
+    PlanViewSet,
+    SubscriptionViewSet
 )
 
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register("apps", AppViewSet, basename="apps")
+router.register("plans", PlanViewSet, basename="plans")
+router.register("subscription", SubscriptionViewSet, basename="subscription")
 
 urlpatterns = [
     path("", include(router.urls)),
